5b2feeec34f263e8a62eabd0650ba6a47ef1b6dd
Shubhrasankharoy <shubhrasankharoy@gmail.com>
2025-09-07 00:34:05 +0530

    Mistake

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='w-[99%] bg-white mx-auto p-3 rounded-lg shadow-lg'>
             <h2 className='madimi-one-regular relative after:absolute after:left-0 after:bottom-[4px] after:h-[2px] after:w-full after:bg-[#ff4949] w-fit text-3xl'>
-                <span>CARRER</span>
+                <span>CAREER</span>
                 <span className='text-[#ff4949]'>RASTA</span>
             </h2>
         </div>
