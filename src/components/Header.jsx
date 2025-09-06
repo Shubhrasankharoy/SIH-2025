896e4521611a65ba7de9330b8946fd2dfbb9983a
Shubhrasankharoy <shubhrasankharoy@gmail.com>
2025-09-07 00:35:25 +0530

    Change the name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
     return (
         <div className='w-[99%] bg-white mx-auto p-3 rounded-lg shadow-lg'>
             <h2 className='madimi-one-regular relative after:absolute after:left-0 after:bottom-[4px] after:h-[2px] after:w-full after:bg-[#ff4949] w-fit text-3xl'>
-                <span>CAREER</span>
-                <span className='text-[#ff4949]'>RASTA</span>
+                <span>SHIKSHA </span>
+                <span className='text-[#ff4949]'>SAARTHI</span>
             </h2>
         </div>
     )
